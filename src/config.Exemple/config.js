f07a666e81a99b33d57aae9f9bcd5bcc7d0aefca
Danilo Ramalho Silva <daniloti@hotmail.com>
2022-12-04 13:35:51 -0300

    Initial commit

---
+++ src/config.Exemple/config.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+
+const environment = process.env.NODE_ENV || 'test';
+const nameBD = 'nomeBd';
+
+const suffix = {
+  dev: '-dev',
+  development: '',
+  test: '-test',
+};
+
+const options = {
+  host: process.env.MYSQL_HOST || 'localhost',
+  port: process.env.MYSQL_PORT || '3306',
+  database:
+    `${process.env.MYSQL_DB_NAME || nameBD}${suffix[environment] || suffix.test}`,
+  username: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD || '1234',
+  dialect: 'mysql',
+  dialectOptions: { timezone: 'Z' },
+  logging: process.env.DEBUG !== 'false',
+};
+
+module.exports = {
+  development: { ...options },
+  test: { ...options },
+};
