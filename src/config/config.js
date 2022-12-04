3ae3878d630f0ec78c0691cbbf6fe9d4612630f3
Danilo Ramalho <daniloti@hotmail.com>
2022-12-04 17:06:43 -0300

    Configurando sequelize

--- src/config/config.js
+++ src/config/config.js
@@ -1,19 +1,18 @@
 require('dotenv').config();
 
-const environment = process.env.NODE_ENV || 'test';
-const nameBD = 'nomeBd';
+const environment = process.env.NODE_ENV || 'dev';
+const nameBD = 'score_bd';
 
 const suffix = {
   dev: '-dev',
-  development: '',
+  development: '-pro',
   test: '-test',
 };
 
 const options = {
   host: process.env.MYSQL_HOST || 'localhost',
   port: process.env.MYSQL_PORT || '3306',
-  database:
-    `${process.env.MYSQL_DB_NAME || nameBD}${suffix[environment] || suffix.test}`,
+  database: `${process.env.MYSQL_DB_NAME || nameBD}${suffix[environment] || suffix.test}`,
   username: process.env.MYSQL_USER || 'root',
   password: process.env.MYSQL_PASSWORD || '1234',
   dialect: 'mysql',
