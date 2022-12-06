27970f50fe7b4da858f2f0324dbc7f2d6d744ba8
Danilo Ramalho <daniloti@hotmail.com>
2022-12-06 15:37:39 -0300

    Refatorando seed da tabela de usuarios

--- src/seeders/20221205173352-users.js
+++ src/seeders/20221205173352-users.js
@@ -7,14 +7,14 @@ module.exports = {
         name: 'danilo',
         password: '',
         image: 'https://upload.wikimedia.org/wikipedia/commons/1/18/Lewis_Hamilton_2016_Malaysia_2.jpg',
-        creatDate: new Date(),
+        creat_date: new Date(),
       },
       {
         id: 2,
         name: 'Michael Schumacher',
         password: '123456',
         image: 'https://sportbuzz.uol.com.br/media/_versions/gettyimages-52491565_widelg.jpg',
-        creatDate: new Date(),
+        creat_date: new Date(),
       },
       ], { timestamps: false });
   },
