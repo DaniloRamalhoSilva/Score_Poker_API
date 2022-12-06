d6e2ecda126cddba2475b9990e6bbbfefe30d72f
Danilo Ramalho <daniloti@hotmail.com>
2022-12-06 14:13:53 -0300

    Criando tabela de partidas

---
+++ src/migrations/20221206165401-create-match.js
@@ -0,0 +1,24 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => queryInterface.createTable('match', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER,
+    },
+    tableId: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      field: 'table_id',
+      references: {
+        model: 'tables',
+        key: 'id',
+      },
+    },
+  }),
+
+  // eslint-disable-next-line no-unused-vars
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('match');
+  },
+};
