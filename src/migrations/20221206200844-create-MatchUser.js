e830832a1fda5b0ff7071c4b5b1f8e7f3a2f48ac
Danilo Ramalho <daniloti@hotmail.com>
2022-12-06 21:46:26 -0300

    Criando tabela de partidas usuarios

---
+++ src/migrations/20221206200844-create-MatchUser.js
@@ -0,0 +1,37 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('match_users', {
+      matchId: {
+        type: Sequelize.INTEGER,
+        field: 'match_id',
+        references: {
+          model: 'matchs',
+          key: 'id',
+        },
+        primaryKey: true,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        field: 'user_id',
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+        primaryKey: true,
+      },
+      podium: {
+        type: Sequelize.INTEGER,
+        field: 'podium',
+      },
+      scored: {
+        type: Sequelize.INTEGER,
+        field: 'scored',
+      },
+    });
+  },
+
+  // eslint-disable-next-line no-unused-vars
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('Match_users');
+  },
+};
