0fbd1b2459f9dd11c02a54893959483bb89ecfb3
Danilo Ramalho <daniloti@hotmail.com>
2022-12-17 19:38:17 -0300

    usando sem doker

--- src/auth/jwtFunctions.js
+++ src/auth/jwtFunctions.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
 const jwt = require('jsonwebtoken');
 
-const secret = process.env.JWT_SECRET || 'seusecretdetoken';
+const secret = process.env.JWT_SECRET;
 
 const jwtConfig = {
   expiresIn: '1d',
